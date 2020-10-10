c291041171e34c1afb9d427c5943a67c61dfd471
unknown <rbhandari1996@gmail.com>
2020-10-10 17:51:51 +0530

    inportant info

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jaewon:pass@node-rest-shop.fbe9p.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true , useUnifiedTopology: true
-}).then(() =>{
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+const config = require('./config/key')
+
+
+mongoose.connect(config.mongoURI , {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
     console.log('DB connected');
 })
-.catch((err) =>{
-    console.log(err);
-});
+    .catch((err) => {
+        console.log(err);
+    });
+mongoose.set("useCreateIndex", true);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req,res) =>{
-    res.send('hello world')
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) {
+            return res.json({ success: false, err })
+        }
+        res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
 })
 app.listen(5000);
